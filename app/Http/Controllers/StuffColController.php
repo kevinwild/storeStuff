11297802e3f47bac6d3ead4223a7e0af47366a7a
kaw28 <kaw28@njit.edu>
2018-06-13 15:37:05 -0400

    Initial Install

---
+++ app/Http/Controllers/StuffColController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\StuffCol;
+use Illuminate\Http\Request;
+
+class StuffColController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\StuffCol  $stuffCol
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StuffCol $stuffCol)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\StuffCol  $stuffCol
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StuffCol $stuffCol)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\StuffCol  $stuffCol
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StuffCol $stuffCol)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\StuffCol  $stuffCol
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StuffCol $stuffCol)
+    {
+        //
+    }
+}
